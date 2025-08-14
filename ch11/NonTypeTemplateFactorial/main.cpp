4cc77e636e510f1768f332636456328ba13aac54
Thomas Flanigan <tflanig1@gmail.com>
2025-08-14 19:25:41 -0400

    Ch 11 done

---
+++ ch11/NonTypeTemplateFactorial/main.cpp
@@ -0,0 +1,23 @@
+template <int N>
+constexpr int factorial()
+{
+    static_assert(N >= 0, "factorial(): N must not be negative");
+
+    int result{ 1 };
+    for (int i = 1; i <= N; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int main()
+{
+    static_assert(factorial<0>() == 1);
+    static_assert(factorial<3>() == 6);
+    static_assert(factorial<5>() == 120);
+
+    factorial<-3>(); // should fail to compile
+
+    return 0;
+}
